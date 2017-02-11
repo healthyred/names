30e05feda375734076b8a07f1f439ad5777380b6
jian13579 <jianhlu018@gmail.com>
2017-02-10 20:11:20 -0500

    text files added

---
+++ R/readnames.R
@@ -0,0 +1,11 @@
+#' readnames
+#'
+#' Takes in a datafile of names of Williams college graduates, and then
+#' returns a dataset of the first name, last name, what level of honors,
+#' the major.
+#'
+
+
+readnames <- function(datafile){
+  input <- readLines(datafile)
+}
